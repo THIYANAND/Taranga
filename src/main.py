1d7647446c3b1146fe4f6398bfcce6cfe193dcdd
THIYANAND <anandgameid@gmail.com>
2026-05-07 21:42:07 +0530

    taranga implemented

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from gui.main_window import MainWindow
+from PySide6.QtGui import QIcon
+
 app = QApplication(sys.argv)
 window = MainWindow()
+window.setWindowIcon(
+    QIcon("assets/Taranga_Logo.ico")
+)
 window.showMaximized()
 sys.exit(app.exec())
