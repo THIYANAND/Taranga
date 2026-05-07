826420a1a87b22a0ad5f42431ec608b19b5b0ca6
THIYANAND <anandgameid@gmail.com>
2026-05-07 20:28:50 +0530

    completed the work

--- src/signal_generator.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-def generate_sine_wave(freq=1, amp=1, phase=0, samples=1000):
-    t = np.linspace(0, 1, samples)
-    y = amp * np.sin(2 * np.pi * freq * t + phase)
-    return t, y
